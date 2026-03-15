1f0ddf9249303be93586e3bffe759dd672872672
Om Prakash <bomprakash485@gmail.com>
2026-03-15 21:57:20 +0530

    create register user route successfully

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ app.use(
   }),
 );
 
+//import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/auth", authRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
